2fdf64dd39b6c15eb56cec4de9ae6de2505d72bc
Georgescu-Radu <georgescuradu97@gmail.com>
2023-06-15 11:15:25 +0300

    Secret config removed

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ return array(
     'authorizationRequestUrl' => 'https://appcenter.intuit.com/connect/oauth2',
     'tokenEndPointUrl' => 'https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer',
     'client_id' => 'ABX2kHxLD1okV65NxZC0Omp5p0xM6r81sAdkfIXFrIuUrAGSiU',
-    'client_secret' => 'UEY9LThjy9RAiS5BX3WZs7qWp7VWUer5HaxZnaLk',
+    'client_secret' => 'Put the client secret here',
     'oauth_scope' => 'com.intuit.quickbooks.accounting openid profile email phone address',
     'oauth_redirect_uri' => 'https://localhost:443/callback.php',
 )
